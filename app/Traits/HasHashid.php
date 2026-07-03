2fdfce29947a2c412a5033bac63cdf29a09c23a3
bimaryan <bimaryan046@gmail.com>
2026-07-04 01:21:34 +0700

    feat: add HasHashid trait for model identification via encoded IDs

--- app/Traits/HasHashid.php
+++ app/Traits/HasHashid.php
@@ -15,4 +15,22 @@ trait HasHashid
     {
         return Hashids::encode($this->id);
     }
+
+    /**
+     * Scope untuk mencari model berdasarkan hashid
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $hashid
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function scopeFindByHashidOrFail($query, $hashid)
+    {
+        $decoded = Hashids::decode($hashid);
+        
+        if (empty($decoded)) {
+            abort(404, 'Data tidak ditemukan.');
+        }
+
+        return $query->findOrFail($decoded[0]);
+    }
 }
