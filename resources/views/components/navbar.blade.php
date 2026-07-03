340f138bf92356d7499c55654385e4a28a5c0a91
bimaryan <bimaryan046@gmail.com>
2026-07-04 02:26:59 +0700

    Fix: Perbaiki tinggi sidebar di perangkat mobile menggunakan 100dvh dan penambahan padding bottom

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -135,9 +135,9 @@
 </nav>
 
 <aside id="logo-sidebar"
-    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0"
+    class="fixed top-0 left-0 z-40 w-64 h-[100dvh] pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0"
     aria-label="Sidebar">
-    <div class="h-full px-3 pb-4 mt-3 overflow-y-auto bg-white">
+    <div class="h-full px-3 pb-24 mt-3 overflow-y-auto bg-white">
         <ul class="space-y-2 font-medium">
 
             @php
