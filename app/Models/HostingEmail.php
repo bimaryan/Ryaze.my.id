56c4ec617e78df488fab3c671eb4c040259afcb7
bimaryan <bimaryan046@gmail.com>
2026-07-08 03:55:01 +0700

    feat: create HostingEmail Eloquent model with fillable attributes and user relationship

--- app/Models/HostingEmail.php
+++ app/Models/HostingEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class HostingEmail extends Model
 {
-    use HasFactory;
+    use HasFactory, \App\Traits\HasHashid;
 
     protected $fillable = [
         'user_id',
