6d49f6891b2bbe74e6d5245bfbe5de706081d29a
bimaryan <bimaryan046@gmail.com>
2026-07-29 00:04:00 +0700

    chore: update Nginx proxy configuration to use HTTPS and include additional forwarding headers

--- ryaze_ssl_worker.sh
+++ ryaze_ssl_worker.sh
@@ -133,11 +133,16 @@ server {
     ssl_certificate_key /www/ssl/$DOMAIN/privkey.pem;
 
     location / {
-        proxy_pass http://127.0.0.1;
+        proxy_pass https://127.0.0.1;
+        proxy_ssl_server_name on;
+        proxy_ssl_name $PROJECT_DOMAIN;
+        proxy_ssl_verify off;
+        
         proxy_set_header Host $PROJECT_DOMAIN;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-Proto https;
+        proxy_set_header X-Forwarded-Host \$host;
     }
 }
 EOF
