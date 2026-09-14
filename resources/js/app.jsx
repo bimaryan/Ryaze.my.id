19fc5471e3ff9829359931d386acab663bcd5695
bimaryan <bimaryan046@gmail.com>
2026-09-14 21:33:47 +0700

    feat: add application entry point for Inertia.js and React setup

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -8,7 +8,7 @@ import { createRoot } from 'react-dom/client';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-    resolve: (name) => resolvePageComponent(`../Pages/${name}.jsx`, import.meta.glob('../Pages/**/*.jsx')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
