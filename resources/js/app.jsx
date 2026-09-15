fd14e4f1fe0bd9e96d1452598690581329a4213c
bimaryan <bimaryan046@gmail.com>
2026-09-15 09:25:10 +0700

    feat: integrate Inertia.js with React and Ziggy routing

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,6 +6,9 @@ window.Quill = Quill;
 import { createInertiaApp } from '@inertiajs/react';
 import { createRoot } from 'react-dom/client';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { route } from 'ziggy-js';
+
+window.route = route;
 
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
