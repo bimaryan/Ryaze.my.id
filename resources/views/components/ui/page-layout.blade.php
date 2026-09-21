bf13f989af9add92146f47553638333d2546b25f
bimaryan <bimaryan046@gmail.com>
2026-09-22 02:51:57 +0700

    p

--- resources/views/components/ui/page-layout.blade.php
+++ resources/views/components/ui/page-layout.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 mt-3 sm:ml-64">
+<div class="p-4 mt-3 transition-all duration-300 ease-in-out" :class="$store.sidebar.collapsed ? 'sm:ml-20' : 'sm:ml-64'">
     <div class="space-y-4 rounded-lg mt-14 transition-opacity duration-300" id="pjax-container">
         {{ $slot }}
     </div>
